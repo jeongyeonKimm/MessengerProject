aa1356f12c8dcf6995d39c5464ccbb675dcbf8c3
kim_jeongyeon <wjddus9132@gmail.com>
2022-08-05 23:17:18 +0900

    update domain(2)

--- src/main/java/study/sns/domain/TimeBaseEntity.java
+++ src/main/java/study/sns/domain/TimeBaseEntity.java
@@ -3,13 +3,12 @@ package study.sns.domain;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.MappedSuperclass;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
 public class TimeBaseEntity {
